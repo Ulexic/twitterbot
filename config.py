1c720e4fac530a208bc0d14072b362798687d25a
Lucie Roux <lucie.roux@supdevinci-edu.fr>
2023-11-24 21:19:22 +0100

    Lyrics & stats

---
+++ config.py
@@ -0,0 +1,4 @@
+api_key = "xblfZB3pg5kSlT1mk2iycUdn3"
+api_key_secret = "hN1uTJIY0LMf2o2chAd2a6FYgMfRALlF1WdCv5rUW4rn8v20NT"
+access_token = "1681960706265038850-4n1mwl7TyVVWFKtXqKfIwoy05UvhoK"
+access_token_secret = "GNJFQC3ERncbIhWc972opeAr6tMJz8koPH6XkPWStwDpy"
